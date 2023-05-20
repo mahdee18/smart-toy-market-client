0ee8e514d37807cfa4e1673460a64df1cd0cde01
mahdee18 <mahdeec10@gmail.com>
2023-05-20 15:00:29 +0600

    Banner section updated

---
+++ src/pages/Home/Banner/Banner.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Banner = () => {
+  return (
+    <div>
+      <div className="container mx-auto relative">
+        <img className="w-full h-[500px] object-cover" src="https://cdn.shopify.com/s/files/1/0475/2385/6541/files/slide-1.jpg?v=1613706860" alt="banner" />
+        <div className="absolute top-0 w-full h-full bg-black opacity-10"></div>
+        <div className="absolute top-1/2 right-5 transform -translate-y-1/2 text-gray-500 text-center sm:text-right lg:text-center lg:max-w-lg lg:pr-10">
+          <h1 className="text-5xl font-bold">
+            Welcome to <br />
+            <span className='text-blue-500'>Smart Toy Market</span>
+          </h1>
+          <p className="py-6 text-white">
+            Explore our wide range of innovative and <br />
+            educational toys for engineering, language learning, and more!
+          </p>
+          <button className="btn btn-active">Get Started</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import GallerySection from '../GallerySection/GallerySection';
 import Category from '../Category/Category';
+import Banner from '../Banner/Banner';
 
 const Home = () => {
     return (
         <div>
+            <Banner></Banner>
             <GallerySection></GallerySection>
             <Category></Category>
         </div>
