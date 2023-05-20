f0d9ffa5f8ec117ab9dea3a25e1acb8610db230d
mahdee18 <mahdeec10@gmail.com>
2023-05-21 00:08:10 +0600

    Update product details

--- src/pages/Shared/ProductDetails/ProductDetails.jsx
+++ src/pages/Shared/ProductDetails/ProductDetails.jsx
@@ -3,11 +3,26 @@ import { useLoaderData } from 'react-router-dom';
 
 const ProductDetails = () => {
     const toy = useLoaderData()
+    const { category, picture, name, price, rating, quantity, seller, description, email } = toy
     console.log(toy)
     return (
-        <div>
-            <h2>This is product details</h2>
+        <div className=" mx-auto bg-white rounded-lg shadow-md overflow-hidden w-full flex items-center">
+            <img className="w-1/2 object-cover" src={picture} alt={name} />
+            <div className="px-4 py-4 w-1/2">
+                <h1 className="text-2xl font-bold  text-gray-800">{name}</h1>
+                <div className="text-gray-500 text-lg mt-2">
+                    <p className=" mt-2"><strong>Category:</strong> {category}</p>
+                    <p className="py-2"><strong>Price:</strong> {price}</p>
+                    <p className=""><strong>Rating:</strong> {rating}</p>
+                    <p className="py-2"><strong>Quantity: </strong>{quantity}</p>
+                    <p className=""><strong>Seller: </strong>{seller}</p>
+                    <p className="py-2"><strong>Description:</strong> {description}</p>
+                    <p className=""><strong>Email: </strong>{email}</p>
+                </div>
+            </div>
         </div>
+
+
     );
 };
 
