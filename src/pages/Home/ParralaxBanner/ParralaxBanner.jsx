ca1cc3481a3d8af713c5036c40b73bb99c7255bb
mahdee18 <mahdeec10@gmail.com>
2024-01-11 23:44:25 +0600

    Make more mobile optimize

--- src/pages/Home/ParralaxBanner/ParralaxBanner.jsx
+++ src/pages/Home/ParralaxBanner/ParralaxBanner.jsx
@@ -8,7 +8,7 @@ const ParralaxBanner = () => {
                 <div className="absolute inset-0 bg-black opacity-70"></div>
                 <div className="absolute inset-0 flex flex-col justify-center items-center text-white">
                     <h2 className="text-xl font-semibold">BUY YOUR TOY FROM US</h2>
-                    <h1 className="text-4xl font-bold my-5">CHOOSE YOUR BEST TOY FROM SAMRT TOY MARKET</h1>
+                    <h1 className="text-2xl lg:text-4xl font-bold my-5">CHOOSE YOUR BEST TOY FROM SAMRT TOY MARKET</h1>
                     <Link to='/allToys' className='btn bg-[#ff6e13] text-white hover:bg-indigo-500'>Get Start Now!</Link>
                 </div>
             </div>
