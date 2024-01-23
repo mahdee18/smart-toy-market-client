804bd632f724de2e79fe21f23b77d0b2b4eda1e5
mahdee18 <mahdeec10@gmail.com>
2024-01-23 23:51:13 +0600

    About Section added

--- src/pages/Home/Facilities/Facilities.jsx
+++ src/pages/Home/Facilities/Facilities.jsx
@@ -1,67 +1,33 @@
 import React from 'react';
-import './Facilities.css'
-import { FaChalkboardTeacher, FaPhotoVideo } from 'react-icons/fa';
-import { AiOutlineFundProjectionScreen } from 'react-icons/ai';
-import { GiFlexibleLamp } from 'react-icons/gi';
-import { Fade } from 'react-awesome-reveal';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+AOS.init();
 
 const Facilities = () => {
     return (
-        <div className='grid md:grid-cols-4 gap-4 my-10 px-6 md:px-14'>
-            <div  className='bg-gray-200 text-center border p-5 shadow-xl hover:shadow-xl hover:shadow-[#ff6e13] parent-div'>
-                <Fade direction='right' triggerOnce>
-                    <div className='flex flex-col items-center justify-center'>
-                        <div className='mb-4'>
-                            <FaPhotoVideo  className='text-[80px] transition-transform duration-300 transform-gpu icon'></FaPhotoVideo >
-                        </div>
-                        <div>
-                            <h1 className='text-2xl font-bold my-3'>Learning Through Play</h1>
-                            <p>Engaging Toys for Holistic and Creative Child Development</p>
-                        </div>
-                    </div>
-                </Fade>
+        <div className='my-16 px-6 md:px-14'>
 
-            </div>
-            <div  className='bg-gray-200 text-center border p-5 shadow-xl hover:shadow-xl hover:shadow-[#ff6e13] parent-div'>
-                <Fade direction='up' triggerOnce>
-                    <div className='flex flex-col items-center justify-center'>
-                        <div className='mb-4'>
-                            <FaChalkboardTeacher className='text-[80px] transition-transform duration-300 transform-gpu icon'></FaChalkboardTeacher>
-                        </div>
-                        <div>
-                            <h1 className='text-2xl font-bold my-3'>Curated Educational Kits</h1>
-                            <p>Inspiring Creativity and Critical Thinking with Thoughtfully Designed Learning Kits.</p>
-                        </div>
-                    </div>
-                </Fade>
-
-            </div>
-            <div  className=' bg-gray-200 text-center border p-5 shadow-xl hover:shadow-xl hover:shadow-[#ff6e13] parent-div'>
-                <Fade direction='down' triggerOnce>
-                    <div className='flex flex-col items-center justify-center'>
-                        <div className='mb-4'>
-                            <AiOutlineFundProjectionScreen className='text-[80px] transition-transform duration-300 transform-gpu icon'></AiOutlineFundProjectionScreen>
-                        </div>
-                        <div>
-                            <h1 className='text-2xl font-bold my-3'>STEM-Focused Fun</h1>
-                            <p>Explore Science, Technology, Engineering, and Math Through Exciting Educational Toys.</p>
-                        </div>
-                    </div>
-                </Fade>
-
-            </div>
-            <div  className='bg-gray-200 text-center border p-5 shadow-xl hover:shadow-xl hover:shadow-[#ff6e13] parent-div'>
-                <Fade direction='left' triggerOnce>
-                    <div className='flex flex-col items-center justify-center'>
-                        <div className='mb-4'>
-                            <GiFlexibleLamp className='text-[80px] transition-transform duration-300 transform-gpu icon'></GiFlexibleLamp>
-                        </div>
-                        <div>
-                            <h1 className='text-2xl font-bold my-3'>Age-Appropriate Learning Tools</h1>
-                            <p>Tailored Toys for Every Stage, Nurturing Growth and Exploration.</p>
-                        </div>
-                    </div>
-                </Fade>
+            <div className='grid md:grid-cols-4 gap-4'>
+                <div data-aos="zoom-in" className='text-center p-10 rounded shadow-xl space-y-3'>
+                    <img className='mx-auto' src='https://cdn1.iconfinder.com/data/icons/big-data-orchid-vol-1/256/Feature_Selection-256.png' alt="" />
+                    <h1 className='text-2xl font-bold'>Big Selection</h1>
+                    <p>The widest toy range.</p>
+                </div>
+                <div data-aos="zoom-in" className='text-center p-10 rounded shadow-xl space-y-3'>
+                    <img className='mx-auto' src='https://5.imimg.com/data5/SE/KF/MY-23219015/e-commerce-website-500x500.png' alt="" />
+                    <h1 className='text-2xl font-bold'>Online Store</h1>
+                    <p>Easiest online shopping.</p>
+                </div>
+                <div data-aos="zoom-in" className='text-center p-10 rounded shadow-xl space-y-3'>
+                    <img className='mx-auto' src='https://th.bing.com/th/id/OIP.LVWEW_dDEj6rr_Fj3DxnTwHaHa?rs=1&pid=ImgDetMain' alt="" />
+                    <h1 className='text-2xl font-bold'>Delivery</h1>
+                    <p>Fast and easy delivery.</p>
+                </div>
+                <div data-aos="zoom-in" className='text-center p-10 rounded shadow-xl space-y-3'>
+                    <img className='mx-auto' src='https://th.bing.com/th/id/OIP.GOMfpYXn7tz31xp25z96XQAAAA?rs=1&pid=ImgDetMain' alt="" />
+                    <h1 className='text-2xl font-bold'>Support</h1>
+                    <p>Call our awesome team.</p>
+                </div>
             </div>
         </div>
     );
