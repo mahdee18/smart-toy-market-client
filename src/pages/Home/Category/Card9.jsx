bd91dc91964da73f7c4123dad47f154f15e787dd
mahdee18 <mahdeec10@gmail.com>
2024-01-25 23:41:58 +0600

    change category card UI

--- src/pages/Home/Category/Card9.jsx
+++ src/pages/Home/Category/Card9.jsx
@@ -7,21 +7,17 @@ import { Rating } from "@mui/material";
 const Card9 = ({ toy }) => {
   const { user } = useContext(AuthContext);
   return (
-    <div className="max-[350px] md:w-[350px] bg-slate-300/20 px-6 py-4 mx-auto rounded-2xl space-y-6 shadow-md">
+    <div className="max-[350px] md:w-[350px] bg-slate-300/20 px-6 py-4 mx-auto rounded-2xl space-y-3 shadow-md">
       {/* Card Image */}
       <img className="w-[350px] h-[190px] bg-gray-400 rounded-2xl" src={toy.picture} alt="" />
       {/* Card Heading */}
-      <div className="space-y-2">
+      <div className="">
         <h2 className="text-slate-800 font-medium md:text-xl sm:text-lg ">{toy.name}</h2>
         {/* rating  */}
-        <p className='flex gap-2 items-center'><Rating name="half-rating-read" defaultValue={toy.rating} precision={0.1} readOnly />  <span>{toy.rating}</span></p>
-
-
-
-
+        <p className='flex gap-2 items-center '><Rating name="half-rating-read" defaultValue={toy.rating} precision={0.1} readOnly /> <span>{toy.rating}</span></p>
       </div>
       {/* Price and action button */}
-      <div className="mt-5 flex justify-between items-center font-medium">
+      <div className="flex justify-between items-center font-medium">
         <h2 className="md:text-xl text-gray-800">${toy.price}</h2>
         {user ? (
           <Link to={`/categories/${toy._id}`}>
