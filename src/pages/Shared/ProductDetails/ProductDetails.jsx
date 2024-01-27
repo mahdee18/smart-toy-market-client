0cc18d5f8a0d82c8263c87d81bdce46da0b9d7d6
mahdee18 <mahdeec10@gmail.com>
2024-01-27 23:22:25 +0600

    Modify Product details

--- src/pages/Shared/ProductDetails/ProductDetails.jsx
+++ src/pages/Shared/ProductDetails/ProductDetails.jsx
@@ -1,3 +1,4 @@
+import { Rating } from '@mui/material';
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
 
@@ -6,18 +7,18 @@ const ProductDetails = () => {
     const { category, picture, name, price, rating, quantity, seller, description, email } = toy
     console.log(toy)
     return (
-        <div className=" mt-10 mx-auto bg-white rounded-lg shadow-md overflow-hidden w-full flex items-center justify-center">
+        <div className=" mt-10 mx-auto bg-white rounded-lg shadow-md overflow-hidden w-full grid md:grid-cols-2 gap-0 justify-items-center items-center">
             <img className="w-[400px] object-cover h-[400px]" src={picture} alt={name} />
-            <div className="px-4 py-4 w-1/2">
-                <h1 className="text-2xl font-bold  text-gray-800">{name}</h1>
+            <div className="px-4 py-2 w-1/2">
+                <h1 className="text-3xl font-bold  text-gray-800">{name}</h1>
                 <div className="text-gray-500 text-lg mt-2">
+                    <p className="py-2 text-2xl"><strong> ${price}</strong></p>
+                    <p className="font-semibold"> {description}</p>
                     <p className=" mt-2"><strong>Category:</strong> {category}</p>
-                    <p className="py-2"><strong>Price:</strong> ${price}</p>
-                    <p className=""><strong>Rating:</strong> {rating}</p>
+                    <p className=""><strong>Rating:</strong> {rating} <Rating></Rating></p>
                     <p className="py-2"><strong>Quantity: </strong>{quantity}</p>
                     <p className=""><strong>Seller: </strong>{seller}</p>
                     <p className="py-2"><strong>Email: </strong>{email}</p>
-                    <p className=""><strong>Description:</strong> {description}</p>
                 </div>
             </div>
         </div>
